c62e0264a522b2ac396d0c27e6ef898c10ba3b95
jinwon Kwon <kjw55523@gmail.com>
2022-03-24 23:01:06 +0900

    #2.0 Canvas Events

--- js/app.js
+++ js/app.js
@@ -1,2 +1,7 @@
-const jsCanvas = document.getElementById("jsCanvas");
+const canvas = document.getElementById("jsCanvas");
 
+function onmouseMove(event){
+    console.log(event);
+}
+
+    canvas.addEventListener("mousemove", onmouseMove);
