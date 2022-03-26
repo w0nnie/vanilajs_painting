8429804edbc08397a0c6dc0672bc4f9147505c8c
jinwon Kwon <kjw55523@gmail.com>
2022-03-26 15:54:25 +0900

    #2.1 2D Context

--- js/app.js
+++ js/app.js
@@ -1,13 +1,30 @@
 const canvas = document.getElementById("jsCanvas");
+var ctx = canvas.getContext('2d');
+
+canvas.width = 700;
+canvas.height = 700;
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
+
+function startPainting(){
+    painting = true;
+}
 function stopPainting(){
     painting = false;
 }
 function onmouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    }else{
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
 }
 
 function onmouseDown(event){
@@ -15,12 +32,7 @@ function onmouseDown(event){
     console.log(painting);
 }
 
-function onmouseUp(event){
-    stopPainting();
-    console.log(painting);
-}
-
 canvas.addEventListener("mousemove", onmouseMove);
 canvas.addEventListener("mousedown", onmouseDown);
-canvas.addEventListener("mouseup", onmouseUp);
+canvas.addEventListener("mouseup", stopPainting);
 canvas.addEventListener("mouseleave",stopPainting);
