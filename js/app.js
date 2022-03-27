1046e6d4733ae772bbcc4044065b01a17065918b
jinwon Kwon <kjw55523@gmail.com>
2022-03-27 23:06:22 +0900

    #2.3 Changing Color

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 const canvas = document.getElementById("jsCanvas");
 var ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 700;
 canvas.height = 700;
-ctx.strokeStyle = "#2c2c2c";
+ctx.strokeStyle = "#FF3B30";
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -18,12 +19,14 @@ function stopPainting(){
 function onmouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
-    if(!painting){
-        ctx.beginPath();
-        ctx.moveTo(x,y);
-    }else{
-        ctx.lineTo(x,y);
-        ctx.stroke();
+    if(!painting){ //경로를 만듬
+        // console.log("creating  Path Line " ,x,y);
+        ctx.beginPath(); //경로 생성
+        ctx.moveTo(x,y); //선 시작 좌표
+    }else{ // 그림
+        // console.log("creating Line " ,x,y);
+        ctx.lineTo(x,y); //선 끝 좌표
+        ctx.stroke(); //선 
     }
 }
 
@@ -36,3 +39,4 @@ canvas.addEventListener("mousemove", onmouseMove);
 canvas.addEventListener("mousedown", onmouseDown);
 canvas.addEventListener("mouseup", stopPainting);
 canvas.addEventListener("mouseleave",stopPainting);
+console.log(Array.from(colors)); // object로부터 array를 만듬
